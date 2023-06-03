5979dd52d9155e75a83c50a61ae2d0b88fcc0603
yuri2 <yuri2peter@qq.com>
2023-06-03 10:29:25 +0800

    init from wac

---
+++ frontend/src/configs.ts
@@ -0,0 +1,10 @@
+export const IS_PROD = import.meta.env.PROD;
+export const IS_DEV = !IS_PROD;
+export const SERVER_ORIGIN = IS_PROD ? '' : getDefaultServerOrigin();
+export const USE_SOCKET = false;
+export const USE_SPA = true;
+
+function getDefaultServerOrigin(port = 3000) {
+  const { protocol, hostname } = window.location;
+  return `${protocol}//${hostname}:${port}`;
+}
